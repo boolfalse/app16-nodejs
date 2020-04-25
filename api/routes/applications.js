17bf450daeff43a1bed518cb53415ae138ad9eea
boolfalse <boolfalse@mail.ru>
2020-04-25 22:28:28 +0400

    Current Application

--- api/routes/applications.js
+++ api/routes/applications.js
@@ -25,7 +25,19 @@ function init() {
 }
 
 async function getCurrentApplication(req, res) {
+    if (_isEmpty(req.query)) {
+        return response.errorWithFields(res, 422, "Device token was not entered!", [
+            'device_token'
+        ]);
+    }
+
     const deviceToken = req.query.device_token;
+    // TODO: Add and use config variable for "device_token"
+    if (validator.isEmpty(deviceToken) || deviceToken.length < 3) {
+        return response.errorWithFields(res, 422, "Device token not valid!", [
+            'device_token'
+        ]);
+    }
 
     const application = await applicationController.getCurrentApplication(deviceToken);
     if (_isEmpty(application)) {
